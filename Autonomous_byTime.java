048e3d7cab51bfbd2b981f9a26214ccf382896e1
Inpulse <ftcimpulse1566@gmail.com>
2025-02-01 20:27:50 +0300

    For SPb championship step 1 Version 2.2.1

--- Autonomous_byTime.java
+++ Autonomous_byTime.java
@@ -6,25 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-/*
- * This OpMode illustrates the concept of driving a path based on time.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backward for 1 Second
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 @Autonomous(name="Autonomous_byTime")
 public class Autonomous_byTime extends LinearOpMode {
     // Указываем 4 мотора
