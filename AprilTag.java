05009a0f50c691292101a1398706ed1ca0174c6f
Impulse <ftcimpulse1566@gmail.com>
2025-05-26 21:46:57 +0300

    Update Version 4.6

--- AprilTag.java
+++ AprilTag.java
@@ -2,9 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import android.annotation.SuppressLint;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -14,35 +18,30 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
 import java.util.List;
-
 @TeleOp(name = "ApriltagRead")
 public class AprilTag extends LinearOpMode {
-
-    private static final boolean USE_WEBCAM = true;
+    private static final boolean USE_WEBCAM = false; //false для использования вебкамеры телефона
     private Position cameraPosition = new Position(DistanceUnit.INCH,
             0, 0, 0, 0);
     private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
             0, -90, 0, 0);
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
-
+    Telemetry tm;
     @Override
     public void runOpMode() {
-
+        tm = FtcDashboard.getInstance().getTelemetry();
         initAprilTag();
-
-        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
-        telemetry.addData(">", "Touch START to start OpMode");
-        telemetry.update();
+        tm.addData("DS preview on/off", "3 dots, Camera Stream");
+        tm.addData(">", "Touch START to start OpMode");
+        tm.update();
         waitForStart();
-
         while (opModeIsActive()) {
 
             telemetryAprilTag();
 
-            telemetry.update();
+            tm.update();
 
             if (gamepad1.dpad_down) {
                 visionPortal.stopStreaming();
@@ -81,27 +80,27 @@ public class AprilTag extends LinearOpMode {
     private void telemetryAprilTag() {
 
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        tm.addData("# AprilTags Detected", currentDetections.size());
 
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
+                tm.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                tm.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
                         detection.robotPose.getPosition().x,
                         detection.robotPose.getPosition().y,
                         detection.robotPose.getPosition().z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
+                tm.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
                         detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
                         detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
                         detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
             } else {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                tm.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                tm.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
             }
         }
 
-        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        tm.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        tm.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
 
     }
 
